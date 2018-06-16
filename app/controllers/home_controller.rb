33be58c4e10c008af1989cd35c457858ff36fbb4
Mai Lan <mailan.peng@gmail.com>
2018-06-16 14:00:59 +0000

    week 4

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,7 @@
   class HomeController < ApplicationController
     def index 
       
-      @name = 'Grumpy Cat'
-      @surname_1 =  'Aussi connu sous le nom de : Tardar Sauce '
-      @surname_2 = 'Ou : Chat grincheux'
-      @descritpion = 'Célébrité du web ravageuseusement boudeuse, mème interstellaire'
-      @comment = 'Ce chat roxe du poney'
-      
-      @curiosites = [
-        'pelote de laine',
-        'tapis de souris',
-        'arbre à gratter',
-        'oreilles de lapin',
-        'peluche dinosaure']
+     @my_curiosities = Curiosity.all
         
         
         
