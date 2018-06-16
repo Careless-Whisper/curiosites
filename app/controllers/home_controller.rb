412073f3a59376c9ad55ee17a9a3c774b9a07fa8
Mai Lan <mailan.peng@gmail.com>
2018-06-16 12:12:24 +0000

    grumpy cat

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,25 @@
-class HomeController < ApplicationController
-  def index
+  class HomeController < ApplicationController
+    def index 
+      
+      @name = 'Grumpy Cat'
+      @surname_1 =  'Aussi connu sous le nom de : Tardar Sauce '
+      @surname_2 = 'Ou : Chat grincheux'
+      @descritpion = 'Célébrité du web ravageuseusement boudeuse, mème interstellaire'
+      @comment = 'Ce chat roxe du poney'
+      
+      @curiosites = [
+        'pelote de laine',
+        'tapis de souris',
+        'arbre à gratter',
+        'oreilles de lapin',
+        'peluche dinosaure']
+        
+        
+        
+        
+      
+    end 
   end
-end
+  
+  
+ 
